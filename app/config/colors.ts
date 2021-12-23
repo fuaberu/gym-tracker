dac440e5f177b95806b26f65ef2f7878cf140425
fuaberu <kevinfabel@gmail.com>
2021-12-23 18:10:59 -0300

    project base colors

--- app/config/colors.ts
+++ app/config/colors.ts
@@ -1,3 +1,33 @@
 export default {
-	primary: '#fff',
+	/* main colors Dark mode */
+	darkBg: '#252238',
+	darkBgLight: `#2c293f`,
+	darkComponentBg: '#2f2c43',
+	darkComponentSecondaryBg: '#3a374d',
+
+	/* main colors Dark mode */
+	lightBg: '#252238',
+	lightBgLight: `#2c293f`,
+	lightComponentBg: '#2f2c43',
+	lightComponentSecondaryBg: '#3a374d',
+
+	/* text */
+	textDarkPrymary: '#fff',
+	textDarkPlaceholder: '#a2a0ad',
+
+	/* gradient */
+	gradient1: '#ff9e00',
+	gradient2: '#ff8500',
+
+	/* accent colors */
+	black: '#1d1d21',
+	darkGrey: '#48455b',
+	gray: '#7b7a7d',
+	lightGray: '#bbbbbc',
+	white: '#fff',
+
+	/* feedback */
+	success: '#88c459',
+	error: '#f5414f',
+	warning: '#ffd137',
 };
