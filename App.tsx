9da9ee4156cb02b7bd8bd8f8319b885ea8662ad3
fuaberu <kevinfabel@gmail.com>
2021-12-24 13:10:46 -0300

    base stack navigation config

--- App.tsx
+++ App.tsx
@@ -1,28 +1,77 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import LoginScreen from './app/screens/LoginScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 import WellcomeScreen from './app/screens/WellcomeScreen';
+import { useEffect, useState } from 'react';
+import colorStyles from './app/config/colors';
 
 const Stack = createStackNavigator();
 
+export interface UserData {
+	userId: string;
+	email: string;
+	fullname: string;
+}
+
 export default function App() {
+	const [user, setUser] = useState(null);
+
+	const getUser = async () => {
+		//from the local storage
+		// try {
+		// 	const jsonValue = await AsyncStorage.getItem('user');
+		// 	if (jsonValue) setUser(JSON.parse(jsonValue));
+		// } catch (e) {
+		// 	// error reading value
+		// }
+	};
+
+	useEffect(() => {
+		getUser();
+	}, []);
+
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen name="Wellcome" component={WellcomeScreen} />
-				<Stack.Screen name="Login" component={LoginScreen} />
-				<Stack.Screen name="Register" component={RegisterScreen} />
+				{user ? (
+					<Stack.Screen
+						name="Wellcome"
+						component={WellcomeScreen}
+						options={{ headerShown: false }}
+					/>
+				) : (
+					<>
+						<Stack.Screen
+							name="Login"
+							component={LoginScreen}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="Sign Up"
+							component={RegisterScreen}
+							options={{
+								headerStyle: styles.headerStyle,
+								headerTintColor: '#fff',
+								headerTitleStyle: {
+									fontWeight: 'bold',
+								},
+							}}
+						/>
+					</>
+				)}
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
 }
 
 const styles = StyleSheet.create({
+	headerStyle: {
+		backgroundColor: colorStyles.backgroundLight,
+	},
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
